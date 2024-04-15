b4fce7032a8ebd6a76f4cd99737ad3ac431137c8
Gorruy <vladimirmimikin@gmail.com>
2024-04-15 13:45:23 +0300

    tests upd

--- tests/tests.c
+++ tests/tests.c
@@ -8,14 +8,63 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <ifaddrs.h>
 
 #include "arg_parser.h"
+#include "sniffer.h"
 #include "helpers.h"
 
 #define NUMBER_OF_TEST_RUNS 100000
 
+int check_arg_parse(void)
+{
+    parsed_args_t args;
+    struct ifaddrs *addrs,*tmp;
 
-int main(int argc, char* argv[] )
+    int argc = 5;
+
+    getifaddrs(&addrs);
+    tmp = addrs;
+    
+    while (tmp) {
+        if (tmp->ifa_addr && tmp->ifa_addr->sa_family == AF_PACKET) {
+            break;
+        }
+        tmp = tmp->ifa_next;
+    }
+
+    char *argv[5] = { "prname", "--interface", tmp->ifa_name, "--ipdest", "1.1.1.1" };
+    
+    freeifaddrs(addrs);
+
+    int res = parse_args( argc, argv, &args );
+    if ( res < 0 ) {
+        return 0;
+    }
+
+    return 1;
+}
+
+int check_valid_ip(void)
+{
+    char* ip = "1.1.1.1";
+    char buf[100];
+    if ( valid_ip(ip, buf) < 0 ) {
+        return 0;
+    }
+    return 1;
+}
+
+int check_valid_port(void)
+{
+    char* port = "1000";
+    if ( valid_port(port) < 0 ) {
+        return 0;
+    }
+    return 1;
+}
+
+int loadtest(int argc, char* argv[] )
 {
     struct sockaddr_in addr;
     parsed_args_t args;
@@ -57,6 +106,25 @@ int main(int argc, char* argv[] )
             perror("Cant send one packet!\n");
         }
     }
+
+    return 1;
+}
+
+int main(int argc, char* argv[] )
+{
+    if ( !check_arg_parse() ) {
+        ERROR_EXIT("Error in arg_parse function!");
+    }
+    if ( !check_valid_ip() ) {
+        ERROR_EXIT("Error in valid_ip function!");
+    }
+    if ( !check_valid_port() ) {
+        ERROR_EXIT("Error in valid_port function!");
+    }
+
+#ifdef LOAD
+    loadtest(argc, argv);
+#endif
 
     return 0;
 }
