13fd18d2eab5b06a1cc6864c3172e61367b19c7e
Gorruy <vladimirmimikin@gmail.com>
2024-04-15 09:33:49 +0300

    script for perf

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,18 +2,54 @@
 
 MY_IP=$(hostname -I | awk '{print $1}');
 
+#!/usr/bin/bash
+
+MY_IP=$(hostname -I | awk '{print $1}');
+
 # load test for profiling
 # sudo perf record -g -F 103 -- ../bin/sniffer --interface lo & \
+# sudo offcputime-bpfcc -df -p `pgrep -nx sniffer` 20 > out.stacks & \
 # sudo ../bin/tests --interface lo --ipdest ${MY_IP} --portdest 1005 & \
 # sleep 20; sudo ../bin/representer;
 # sudo perf script | stackcollapse-perf.pl > out.perf-folded
-# flamegraph.pl out.perf-folded > perf.svg
+# flamegraph.pl --title="CPU Time Flame Graph" out.perf-folded > perf.svg
+# flamegraph.pl --color=io --title="Off-CPU Time Flame Graph" --countname=us < out.stacks > out.svg
 
-# sudo perf record -e sched:sched_stat_sleep -e sched:sched_switch -e sched:sched_process_exit -g -F 103 -- ../bin/sniffer --interface lo & \
-# sudo ../bin/tests --interface lo --ipdest ${MY_IP} --portdest 1005 & \
-# sleep 20; sudo ../bin/representer;
-# sudo perf script | stackcollapse-perf.pl > out.perf-folded
-# flamegraph.pl out.perf-folded > perf.svg
+# Tests to check ipdest:
+sudo ./bin/sniffer --interface lo --ipdest ${MY_IP} > /dev/null & sleep 1;
+echo "IP DEST TEST:" | nc -u -w0 ${MY_IP} 1005;
+res="$(sudo ./bin/representer)" 
+
+if [[ $res != "Number of packets:1, size of all packets in bytes:56" ]]; then
+    echo "Error while checking ipdest!"
+fi
+
+sudo ./bin/sniffer --interface lo --ipdest 100.100.100.100 > /dev/null & sleep 1;
+echo "IP DEST TEST:" | nc -u -w0 ${MY_IP} 1005;
+res="$(sudo ./bin/representer)" 
+
+if [[ $res != "Number of packets:0, size of all packets in bytes:0" ]]; then
+    echo "Error while checking ipdest!"
+fi
+
+# Tests to check portdest:
+sudo ./bin/sniffer --interface lo --portdest 1005 > /dev/null & sleep 1;
+echo "IP DEST TEST:" | nc -u -w0 ${MY_IP} 1005;
+res="$(sudo ./bin/representer)" 
+
+if [[ $res != "Number of packets:1, size of all packets in bytes:56" ]]; then
+    echo "Error while checking portdest!"
+fi
+
+sudo ./bin/sniffer --interface lo --portdest 123 > /dev/null & sleep 1;
+echo "IP DEST TEST:" | nc -u -w0 ${MY_IP} 1005;
+res="$(sudo ./bin/representer)" 
+
+if [[ $res != "Number of packets:0, size of all packets in bytes:0" ]]; then
+    echo "Error while checking portdest!"
+fi
+
+sudo ./bin/tests
 
 # Tests to check ipdest:
 sudo ./bin/sniffer --interface lo --ipdest ${MY_IP} > /dev/null & sleep 1;
