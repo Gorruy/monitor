a72b81dc7ef7d9c5cdc6cf6263089b9067e95057
Gorruy <vladimirmimikin@gmail.com>
2024-04-17 09:06:00 +0300

    flamegrpaph commented, need to run them separately

--- tests/cpu_graph_build.sh
+++ tests/cpu_graph_build.sh
@@ -2,8 +2,8 @@
 
 MY_IP=$(hostname -I | awk '{print $1}');
 
-sudo perf record -g -F 103 -- ../bin/sniffer --interface lo & \
-sudo timeout 20s ../bin/tests --interface lo --ipdest ${MY_IP} --portdest 1005;
-sudo ../bin/representer;
-sudo perf script | stackcollapse-perf.pl > out.perf-folded;
-flamegraph.pl --title="CPU Time Flame Graph" out.perf-folded > perf.svg;
+perf record -g -F 103 -- ../bin/sniffer --interface lo & \
+timeout 20s ../bin/tests --interface lo --ipdest ${MY_IP} --portdest 1005;
+../bin/representer;
+#perf script | stackcollapse-perf.pl > out.perf-folded;
+#flamegraph.pl --title="CPU Time Flame Graph" out.perf-folded > perf.svg;
