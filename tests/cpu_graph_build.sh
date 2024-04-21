a766a7b641ecbec8c2e9ff85365b8771369b3ccf
Gorruy <vladimirmimikin@gmail.com>
2024-04-21 21:42:48 +0300

    upd for prof

--- tests/cpu_graph_build.sh
+++ tests/cpu_graph_build.sh
@@ -5,5 +5,5 @@ MY_IP=$(hostname -I | awk '{print $1}');
 perf record -g -F 103 -- ../bin/sniffer --interface lo & \
 timeout 20s ../bin/tests --interface lo --ipdest ${MY_IP} --portdest 1005;
 ../bin/representer && sudo pkill -2 sniffer;
-#perf script | stackcollapse-perf.pl > out.perf-folded;
+#sudo perf script | stackcollapse-perf.pl > out.perf-folded;
 #flamegraph.pl --title="CPU Time Flame Graph" out.perf-folded > perf.svg;
