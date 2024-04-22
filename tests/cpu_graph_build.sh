bfd40bebad650b268aa16fc5d33b0681ef1abd0d
Gorruy <vladimirmimikin@gmail.com>
2024-04-22 09:10:04 +0300

    100 s prof

--- tests/cpu_graph_build.sh
+++ tests/cpu_graph_build.sh
@@ -3,7 +3,7 @@
 MY_IP=$(hostname -I | awk '{print $1}');
 
 perf record -g -F 103 -- ../bin/sniffer --interface lo & \
-timeout 20s ../bin/tests --interface lo --ipdest ${MY_IP} --portdest 1005;
+timeout 100s ../bin/tests --interface lo --ipdest ${MY_IP} --portdest 1005;
 ../bin/representer && sudo pkill -2 sniffer;
 #sudo perf script | stackcollapse-perf.pl > out.perf-folded;
 #flamegraph.pl --title="CPU Time Flame Graph" out.perf-folded > perf.svg;
--- tests/offcpu_graph_build.sh
+++ tests/offcpu_graph_build.sh
@@ -4,6 +4,6 @@ MY_IP=$(hostname -I | awk '{print $1}');
 
 ../bin/sniffer --interface lo & \
 offcputime-bpfcc -df -p `pgrep -nx sniffer` 20 > out.stacks & \
-timeout 20s ../bin/tests --interface lo --ipdest ${MY_IP} --portdest 1005;
+timeout 100s ../bin/tests --interface lo --ipdest ${MY_IP} --portdest 1005;
 ../bin/representer && sudo pkill -2 sniffer;
 #flamegraph.pl --color=io --title="Off-CPU Time Flame Graph" --countname=us < out.stacks > out.svg;
